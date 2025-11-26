9c126c74c3724bc8f42c26a623174b9cebd2ae55
Nahid Hasan <109328601+anonymousFaisal@users.noreply.github.com>
2025-11-26 10:24:07 +0600

    Created the home page headers and a Navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Schibsted_Grotesk, Martian_Mono } from "next/font/google";
 import "./globals.css";
 import LightRays from "@/components/LightRays";
+import Navbar from "@/components/Navbar";
 
 const schibstedGrotesk = Schibsted_Grotesk({
   variable: "--font-schibsted-grotesk",
@@ -40,6 +41,7 @@ export default function RootLayout({
             className="custom-rays"
           />
         </div>
+        <Navbar />
         {children}
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import ExploreBtn from "../components/ExploreBtn";
 
-function Home() {
+const Home = () => {
   return (
-    <h1>Home</h1>
-  )
-}
+    <section>
+      <h1 className="text-center">
+        The Hub for Every Developer <br /> Event You Can&apos;t Miss
+      </h1>
+      <p className="text-center mt-5">Hackathons, Meetups, and Conferences, All in One Place</p>
+      <ExploreBtn />
+      <div className="mt-20 space-y-7">
+        <h3>Featured Events</h3>
+        <ul className="events">
+          {[1,2,3,4,5].map(event=>(
+            <li key={event}>Event {event}</li>
+          ))}
 
-export default Home
+        </ul>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
