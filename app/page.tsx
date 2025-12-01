3a2584516a2f6de4e78d6b43efd5a5b81cdc9ba0
Nahid Hasan <109328601+anonymousFaisal@users.noreply.github.com>
2025-12-01 19:00:32 +0600

    feat: fetch event get api

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ExploreBtn from "../components/ExploreBtn";
-import EventCard from "../components/EventCard";  
-import { events } from "../lib/constants";
+import EventCard from "../components/EventCard";
+import { IEvent } from "@/database/event.model";
 
-const Home = () => {
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+const Home = async () => {
+  const response = await fetch(`${BASE_URL}/api/events`);
+  const { events } = await response.json();
   return (
     <section>
       <h1 className="text-center">
@@ -14,9 +17,9 @@ const Home = () => {
       <div className="mt-20 space-y-7">
         <h3>Featured Events</h3>
         <ul className="events">
-          {events.map(event=>(
+          {events && events.length > 0 && events.map((event: IEvent) => (
             <li key={event.title}>
-                <EventCard {...event} />
+              <EventCard {...event} />
             </li>
           ))}
         </ul>
