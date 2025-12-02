590206e529d7cc511772760200d35ce7b8cddee1
Nahid Hasan <109328601+anonymousFaisal@users.noreply.github.com>
2025-12-02 19:49:34 +0600

    feat: enable Next.js component caching.

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,12 @@ import React from "react";
 import ExploreBtn from "../components/ExploreBtn";
 import EventCard from "../components/EventCard";
 import { IEvent } from "@/database/event.model";
+import { cacheLife } from "next/cache";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 const Home = async () => {
+  "use cache";
+  cacheLife("hours");
   const response = await fetch(`${BASE_URL}/api/events`);
   const { events } = await response.json();
   return (
